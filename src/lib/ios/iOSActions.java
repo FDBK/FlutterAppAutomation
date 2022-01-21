a278f4b17140715b3b507355f4f4114665c25246
FDBK <single-coil@ya.ru>
2022-01-21 11:16:21 +0700

    iOS Locators updated
    The iOS testing is possible at last! So here are all the new Locators for all the application elements...

--- src/lib/ios/iOSActions.java
+++ src/lib/ios/iOSActions.java
@@ -9,38 +9,38 @@ public class iOSActions extends TrCardActions
     // Присвоение значений константам,
     static {
         // связанным с вводом логина
-        EMAIL_FIELD ="xpath://android.widget.EditText[contains(@text, 'Введите e-mail')]";
-        EMAIL_FIELD_WITH_WARNING ="xpath://android.widget.EditText[contains(@text, 'Некорректный e-mail')]";
+        EMAIL_FIELD ="xpath://XCUIElementTypeTextField[contains(@name, 'Введите e-mail')]";
+        EMAIL_FIELD_WITH_WARNING ="xpath://XCUIElementTypeTextField[contains(@name, 'Некорректный e-mail')]";
         INCORRECT_EMAIL_TEXT ="Некорректный e-mail";
         EMAIL_PATTERN ="\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,10}";
         // связанным с вводом пароля
-        PASSWORD_FIELD ="xpath://android.widget.EditText[contains(@text, 'Введите пароль')]";
-        PASSWORD_FIELD_WITH_WARNING ="xpath://android.widget.EditText[contains(@text, 'Длина пароля минимум') or contains(@text, 'Пароль должен содержать')]";
+        PASSWORD_FIELD ="xpath://XCUIElementTypeTextField[contains(@name, 'Введите пароль')]";
+        PASSWORD_FIELD_WITH_WARNING ="xpath://XCUIElementTypeTextField[contains(@name, 'Длина пароля минимум') or contains(@name, 'Пароль должен содержать')]";
         INCORRECT_PASSWORD_TEXT_v1 ="Длина пароля минимум 8 символов";
         INCORRECT_PASSWORD_TEXT_v2 ="Пароль должен содержать не менее 8 символов";
         // связанным с вводом номера карты
-        CARD_NUMBER_FIELD ="xpath://android.widget.EditText[@text='Номер карты']";
-        CARD_NUMBER_FIELD_WITH_WARNING ="xpath://android.widget.EditText[contains(@text, 'Укажите 19-значный номер карты')]";
+        CARD_NUMBER_FIELD ="xpath://XCUIElementTypeTextField[@name='Номер карты']";
+        CARD_NUMBER_FIELD_WITH_WARNING ="xpath://XCUIElementTypeTextField[contains(@name, 'Укажите 19-значный номер карты')]";
         INCORRECT_CARD_NUMBER_TEXT ="Укажите 19-значный номер карты";
         // связанным с вводом имени карты
-        CARD_NAME_FIELD ="xpath://android.widget.EditText[contains(@text, 'Имя карты')]";
-        CARD_NAME_FIELD_WITH_WARNING ="xpath://android.widget.EditText[contains(@text, 'Укажите имя карты')]";
+        CARD_NAME_FIELD ="xpath://XCUIElementTypeTextField[contains(@name, 'Имя карты')]";
+        CARD_NAME_FIELD_WITH_WARNING ="xpath://XCUIElementTypeTextField[contains(@name, 'Укажите имя карты')]";
         INCORRECT_CARD_NAME_TEXT ="Укажите имя карты длиной";
         // связанным с вводом номера банковской карты
-        CREDIT_CARD_NUMBER_FIELD ="xpath://android.widget.EditText[@text='Номер карты']";
-        CREDIT_CARD_NUMBER_FIELD_WITH_WARNING ="xpath://android.widget.EditText[contains(@text, 'Укажите 16-значный номер карты')]";
+        CREDIT_CARD_NUMBER_FIELD ="xpath://XCUIElementTypeTextField[@name='Номер карты']";
+        CREDIT_CARD_NUMBER_FIELD_WITH_WARNING ="xpath://XCUIElementTypeTextField[contains(@name, 'Укажите 16-значный номер карты')]";
         INCORRECT_CREDIT_CARD_NUMBER_TEXT ="Укажите 16-значный номер карты";
         // связанным с предоставлением приложению разрешений
-        PERMISSION_ALLOW_BUTTON ="xpath://android.widget.Button[contains(@resource-id, 'permission_allow')]";
+        PERMISSION_ALLOW_BUTTON ="xpath://XCUIElementTypeButton[contains(@name, 'НАСТРОЙКИ')]";
         // связанным с шаблонами для поиска элементов
-        TEXT_TEMPLATE ="xpath://android.view.View[contains(@content-desc, '{TEXT}')]";
-        INPUT_FIELD_TEMPLATE ="xpath://android.widget.EditText[contains(@text, '{INPUT_FIELD_TEXT}')]";
-        BUTTON_TEMPLATE ="xpath://android.widget.Button[contains(@content-desc, '{BUTTON_NAME}')]";
-        BUTTON_WITH_PIC_TEMPLATE ="xpath://android.widget.ImageView[contains(@content-desc, '{BUTTON_TEXT}')]";
-        BIG_BUTTON_TEMPLATE ="xpath://android.view.View[contains(@content-desc, '{BIG_BUTTON_TEXT}')]";
-        RADIO_BUTTON_TEMPLATE ="xpath://android.widget.RadioButton[contains(@content-desc, '{RADIO_BUTTON_TEXT}')]";
-        SWITCH_TEMPLATE ="xpath://android.widget.Switch[contains(@content-desc, '{SWITCH_TEXT}')]";
-        TOOLTIP_BUTTON_TEMPLATE ="xpath://android.view.View[contains(@content-desc, '{TOOLTIP_BUTTON_TEXT}')]/android.widget.ImageView[2]";
+        TEXT_TEMPLATE ="xpath://XCUIElementTypeOther[contains(@name, '{TEXT}')]";
+        INPUT_FIELD_TEMPLATE ="xpath://XCUIElementTypeTextField[contains(@name, '{INPUT_FIELD_TEXT}')]";
+        BUTTON_TEMPLATE ="xpath://XCUIElementTypeButton[contains(@name, '{BUTTON_NAME}')]";
+        BUTTON_WITH_PIC_TEMPLATE ="xpath://XCUIElementTypeImage[contains(@name, '{BUTTON_TEXT}')]";
+        BIG_BUTTON_TEMPLATE ="xpath://XCUIElementTypeStaticText[contains(@name, '{BIG_BUTTON_TEXT}')]";
+        RADIO_BUTTON_TEMPLATE ="xpath://XCUIElementTypeSwitch[contains(@name, '{RADIO_BUTTON_TEXT}')]";
+        SWITCH_TEMPLATE ="xpath://XCUIElementTypeSwitch[contains(@name, '{SWITCH_TEXT}')]";
+        TOOLTIP_BUTTON_TEMPLATE ="xpath://XCUIElementTypeOther[contains(@name, '{TOOLTIP_BUTTON_TEXT}')]/../XCUIElementTypeImage[2]";
         ANY_ELEMENT_TEMPLATE ="xpath://*[{ANY_ELEMENT_TEXT}]";
     }
 
